71a5157fd8eef797e199cde6f064bbfdce8abe8a
nanirhapsody <38499893+nanirhapsody@users.noreply.github.com>
2021-07-15 14:02:12 -0300

    Add files via upload

---
+++ solEjerciciosS4U2/ejercicio3.R
@@ -0,0 +1,8 @@
+#Carga los datos que se encuentran a continuación
+library(dslabs)
+data("admissions")
+
+#Realiza una función que reciba el data set admissions 
+#de entrada y devuelva un nuevo data set con el cálculo 
+#del porcentaje de personas admitidas de acuerdo al total 
+#de inscriptos (applicants) para cada especialización. 
---
+++ solEjerciciosS4U2/ejercicio4.R
@@ -0,0 +1,7 @@
+#Carga los datos que se encuentran a continuación
+library(dslabs)
+library(dplyr)
+data("admissions")
+
+#Utilizando la función split separa el dataset en las diferentes especializaciones 
+#y calcula para el grupo de especialización A el promedio de personas admitidas
