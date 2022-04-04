a0de22614a8bdfb360cc7d837c082ba4f7c2af15
nanirhapsody <38499893+nanirhapsody@users.noreply.github.com>
2022-04-04 20:13:34 -0300

    Update ejercicio7.R

--- ejercicios S3U1/ejercicio7.R
+++ ejercicios S3U1/ejercicio7.R
@@ -1,13 +1,13 @@
 library(dslabs) 
 data(murders)
 # Recordemos que si en lugar de un index queremos saber si un elemento 
-#de un vector est· o no en un segundo vector podemos usar %ind% por ej:
+#de un vector est√° o no en un segundo vector podemos usar %in% por ej:
 x <- c(2, 3, 5)
 y <- c(1, 2, 3, 4)
 x%in%y
-#Nos retorna 2 TRUE Y 1 FALSE ya que 2 Y 3 est·n en y pero 5 no
-#Cu·l de las siguientes son abreviaciones de estados MA, ME, MI, MO, MU??   
+#Nos retorna 2 TRUE Y 1 FALSE ya que 2 Y 3 est√°n en y pero 5 no
+#Cu√°l de las siguientes son abreviaciones de estados MA, ME, MI, MO, MU??   
 #Para responder esta pregunta primero guarda las abreviaciones
 #en un vector 'abbs' recordando que son caracteres
 #Utiliza el comando %in% para chequear que entradas de abbs
-# est·n en el data frame de murders
+# est√°n en el data frame de murders
