beddb06eebd15c590d84be7ef2d20145adcce06e
nanirhapsody <38499893+nanirhapsody@users.noreply.github.com>
2022-04-21 10:36:52 -0300

    Update ejercicio2.R

--- ejerciciosS4U2/ejercicio2.R
+++ ejerciciosS4U2/ejercicio2.R
@@ -1,11 +1,10 @@
-#Carga los datos que se encuentran a continuación
+#Carga los datos que se encuentran a continuaciÃ³n
 library(dslabs)
 library(dplyr)
 data("murders")
 
 #Crea un nuevo data frame que contenga el promedio del 
-#índice de asesinatos de las diferentes regiones
+#Ã­ndice de asesinatos de las diferentes regiones
 #Para esto recuerda:
 rate <-murders$total/murders$population * 100000
-#También necesitaras de las funciones mutate , group_by  y summarise( mean )
 
