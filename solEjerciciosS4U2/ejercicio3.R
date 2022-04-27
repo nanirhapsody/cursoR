6a71b28d8517a4f402f3dad07358be9d97f9c18f
nanirhapsody <38499893+nanirhapsody@users.noreply.github.com>
2022-04-27 19:32:42 -0300

    Delete solEjerciciosS4U2 directory

--- solEjerciciosS4U2/ejercicio3.R
+++
@@ -1,8 +0,0 @@
-#Carga los datos que se encuentran a continuación
-library(dslabs)
-data("admissions")
-
-#Realiza una función que reciba el data set admissions 
-#de entrada y devuelva un nuevo data set con el cálculo 
-#del porcentaje de personas admitidas de acuerdo al total 
-#de inscriptos (applicants) para cada especialización. 
