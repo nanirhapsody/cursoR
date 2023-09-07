5faf5d23fb5357e3caa08ada1035d81007eb193a
nanirhapsody <38499893+nanirhapsody@users.noreply.github.com>
2023-09-07 10:56:38 -0300

    Update ejercicio4.R

--- ejercicios S2U1/ejercicio4.R
+++ ejercicios S2U1/ejercicio4.R
@@ -1,4 +1,13 @@
-# el precio de los últimos 3 ítems de nuestras comidas
+# Asociamos la comida a los precios correspondientes 
+cost <- c(50, 75, 90, 100, 150)
+food <- c("pizza", "burgers", "salads", "cheese", "pasta")
+names(cost) <- food
+
+# En los ejercicios anteriores escribiste el cÃ³digo
+temp <- c(35, 88, 42, 84, 81, 30)
+city <- c("Beijing", "Lagos", "Paris", "Rio de Janeiro", "San Juan", "Toronto")
+
+# Para obtener el precio de los Ãºltimos 3 Ã­tems de nuestras comidas:
 cost[3:5]
 
-# Obtener las temperaturas de las 3 primeras ciudades de la lista
+# Escribe el codigo para obtener las temperaturas de las 3 primeras ciudades de la lista
