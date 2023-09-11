68fcf72390e37825ccc6d63abe3281153a5b2a2e
nanirhapsody <38499893+nanirhapsody@users.noreply.github.com>
2023-09-11 13:49:26 -0300

    Update ejercicio3.R

--- ejercicios S2U2/ejercicio3.R
+++ ejercicios S2U2/ejercicio3.R
@@ -1,4 +1,8 @@
-#Encontramos el número de fila del menor total de asesinatos
+#Cargamos las librerias y datos a utilizar
+library(dslabs)
+data(murders)
+
+#Encontramos el nÃºmero de fila del menor total de asesinatos
 which.min(murders$total)
 
-# Encuentra el número de fila de la menor población
+# Encuentra el nÃºmero de fila de la menor poblaciÃ³n
