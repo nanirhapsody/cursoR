6e825bf80bcf53c5de62d83e7ba803cadfa53c5c
nanirhapsody <38499893+nanirhapsody@users.noreply.github.com>
2023-09-28 13:01:03 -0300

    Update ejercicio10.R

--- ejerciciosS4U3/ejercicio10.R
+++ ejerciciosS4U3/ejercicio10.R
@@ -1,4 +1,4 @@
-#Utiliza la función map_df para crear un data frame con 3 columnas que se #llamen n, s_n, y s_n_2
-#La primera debe de contener los números entre 1 y 100
+#Utiliza el paquete purrr para crear un data frame con 3 columnas que se llamen n, s_n, y s_n_2
+#La primera debe de contener los nÃºmeros entre 1 y 100
 #La segunda y tercer columna deben de contener
 #la suma entre 1 y 100 y el numero n de la fila sumada
